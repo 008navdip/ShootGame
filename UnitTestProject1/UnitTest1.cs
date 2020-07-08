5c25f6b41f918a7c4d51a53ade7db0a1db7fdb9a
HP <HP@LAPTOP-L437ARAR>
2020-07-09 11:18:49 +1200

    unit test added

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ShootGame.ShootLogic sl = new ShootGame.ShootLogic();
+
+            Assert.AreNotEqual(sl.FireAwayChance, 3);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ShootGame.ShootLogic sl = new ShootGame.ShootLogic();
+
+            Assert.AreEqual(sl.Bullet, 7);
+
+        }
+    }
+}
